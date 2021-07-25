bfcd6d23fbe78b5b7a64b909a64a513e28d62174
Charles-1414 <36125855+Charles-1414@users.noreply.github.com>
2021-07-25 19:36:53 +0800

    Update config

--- dhtserver.py
+++ dhtserver.py
@@ -4,6 +4,19 @@ from http import server
 import json
 import time
 
+class Dict2Obj(object):
+    def __init__(self, d):
+        for key in d:
+            if type(d[key]) is dict:
+                data = Dict2Obj(d[key])
+                setattr(self, key, data)
+            else:
+                setattr(self, key, d[key])
+
+config_txt = open("./config.json","r").read()
+config = Dict2Obj(json.loads(config_txt))
+motion_output = None
+
 class DHTServer(socketserver.ThreadingMixIn, server.HTTPServer):
     allow_reuse_address = True
     daemon_threads = True
@@ -11,7 +24,7 @@ class DHTServer(socketserver.ThreadingMixIn, server.HTTPServer):
 class DHTHandler(server.BaseHTTPRequestHandler):
     def do_GET(self):
         if self.path == "/info":
-            humidity, temperature = DHT.read_retry(DHT.DHT11, 14)
+            humidity, temperature = DHT.read_retry(DHT.DHT11, config.GPIO.dht)
             if humidity is None:
                 humidity = "--"
             if temperature is None:
