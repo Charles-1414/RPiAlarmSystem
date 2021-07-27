af2612aafd4aa99c6e90a7b6b9b71b5a3927ca3a
Charles-1414 <36125855+Charles-1414@users.noreply.github.com>
2021-07-27 11:11:11 +0800

    Added LAN video deletion function

--- hardware/server.py
+++ hardware/server.py
@@ -1,21 +1,22 @@
 from flask import Flask, send_file, abort
 
 import os
+import json
 
 app = Flask(__name__)
 
 PAGE = """<title>RPiAlarmSystem</title>
 <h1>RPiAlarmSystem Video List</h1>"""
 
-@app.route("/")
+@app.route("/",methods=['GET'])
 def index():
     videos = os.listdir("./videos/")
     page = PAGE
     for video in videos:
-        page += f"<p><a href='/download/{video.replace('.mp4','')}'>{video}</a></p>"
+        page += f"<p><a href='/download/{video.replace('.mp4','')}'>{video}</a> <a href='/delete/{video.replace('.mp4','')}' onclick=\"return confirm('Are you sure to delete this file? This operation cannot be undone!')\">Delete</a></p>"
     return page
 
-@app.route("/download/<string:filename>") # with mp4 extension
+@app.route("/download/<string:filename>",methods=['GET']) # with mp4 extension
 def download_video(filename):
     if not filename.endswith(".mp4"):
         filename+=".mp4"
@@ -23,5 +24,15 @@ def download_video(filename):
         return send_file(f"./videos/{filename}", as_attachment = True)
     else:
         abort(404)
+
+@app.route("/delete/<string:filename>",methods=['GET'])
+def delete_video(filename):
+    if not filename.endswith(".mp4"):
+        filename+=".mp4"
+    if os.path.exists(f"./videos/{filename}"):
+        os.remove(f"./videos/{filename}")
+        return "<meta http-equiv='refresh' content='3;url=/' /><title>RPiAlarmSystem</title><p>File deleted. Redirecting...</p>"
+    else:
+        return "<meta http-equiv='refresh' content='3;url=/' /><title>RPiAlarmSystem</title><p>File not found! Redirecting...</p>"
 
 app.run("0.0.0.0",8080)
